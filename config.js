3eadb45fe4de3d3d641f415f0804ffc484936622
Hung Nguyen <nguyenht33@gmail.com>
2018-08-19 19:17:24 -0400

    updated client origin

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    CLIENT_ORIGIN: 'http://localhost:3000',
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
     DATABASE_URL: process.env.DATABASE_URL || global.DATABASE_URL ||'mongodb://localhost/movie-keeper',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || global.TEST_DATABASE_URL ||'mongodb://localhost/test-movie-keeper',
     PORT: process.env.PORT || 8080,
